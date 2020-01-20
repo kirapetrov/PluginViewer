4b00aa4a08f6d920902acfb39f45e8bc19f412cb
petrov kirill <stimulzzz@gmail.com>
2020-01-21 00:28:43 +0300

    добавлены пост скрипты на копирование плагинов. Так же расширен bootstrapper для загрузки плагинов

--- PluginViewer/PluginViewerBootstrapper.cs
+++ PluginViewer/PluginViewerBootstrapper.cs
@@ -1,15 +1,4 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.ComponentModel.Composition;
-//using System.ComponentModel.Composition.Hosting;
-//using System.ComponentModel.Composition.Primitives;
-//using System.Linq;
-//using System.Reflection;
-//using System.Windows;
-//using Caliburn.Micro;
-//using PluginViewer.ViewModels;
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.ComponentModel.Composition.Hosting;
@@ -34,20 +23,22 @@ namespace PluginViewer
 
         protected override void Configure()
         {
-            // Add all assemblies to AssemblySource (using a temporary DirectoryCatalog).
             var directoryCatalog = new DirectoryCatalog(@"./");
             AssemblySource.Instance.AddRange(
                 directoryCatalog.Parts
                     .Select(part => ReflectionModelServices.GetPartType(part).Value.Assembly)
                     .Where(assembly => !AssemblySource.Instance.Contains(assembly)));
 
-            // Prioritise the executable assembly. This allows the client project to override exports, including IShell.
-            // The client project can override SelectAssemblies to choose which assemblies are prioritised.
+            directoryCatalog = new DirectoryCatalog(@"./Plugins/");
+            AssemblySource.Instance.AddRange(
+                directoryCatalog.Parts
+                    .Select(part => ReflectionModelServices.GetPartType(part).Value.Assembly)
+                    .Where(assembly => !AssemblySource.Instance.Contains(assembly)));
+
             priorityAssemblies = SelectAssemblies().ToList();
             var priorityCatalog = new AggregateCatalog(priorityAssemblies.Select(x => new AssemblyCatalog(x)));
             var priorityProvider = new CatalogExportProvider(priorityCatalog);
 
-            // Now get all other assemblies (excluding the priority assemblies).
             var mainCatalog = new AggregateCatalog(
                 AssemblySource.Instance
                     .Where(assembly => !priorityAssemblies.Contains(assembly))
@@ -65,20 +56,6 @@ namespace PluginViewer
             batch.AddExportedValue(mainCatalog);
 
             container.Compose(batch);
-
-            //var executingAssembly = Assembly.GetExecutingAssembly();
-            //var catalog = new AggregateCatalog();
-            //catalog.Catalogs.Add(new AssemblyCatalog(executingAssembly));
-            //var folder = System.IO.Path.GetDirectoryName(executingAssembly.Location);
-            //catalog.Catalogs.Add(new DirectoryCatalog(folder));
-            //container = new CompositionContainer(catalog);
-            //container.ComposeParts(this);
-
-            //var batch = new CompositionBatch();
-            //batch.AddExportedValue<IWindowManager>(new WindowManager());
-            //batch.AddExportedValue<IEventAggregator>(new EventAggregator());
-            //batch.AddExportedValue(container);
-            //container.Compose(batch);
         }
 
         protected virtual void BindServices(CompositionBatch batch)
